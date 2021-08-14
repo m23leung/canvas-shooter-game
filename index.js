70e0a6b93f039218a8706c5ae0457bd10d007cf9
m23leung <mark.leung89@gmail.com>
2021-08-14 17:58:08 +1000

    Refactored code into separate files

--- index.js
+++ index.js
@@ -8,79 +8,23 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-class Player {
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
-
-  draw() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-  }
-}
-
-class Projectile {
-  constructor(x, y, radius, color, velocity) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-    this.velocity = velocity;
-  }
-
-  draw() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-  }
-
-  update() {
-    this.draw();
-    this.x = this.x + this.velocity.x;
-    this.y = this.y + this.velocity.y;
-  }
-}
-
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
+//const projectiles = [];
 
-const projectile = new Projectile(
-  canvas.width / 2,
-  canvas.height / 2,
-  5,
-  "red",
-  {
-    x: 1,
-    y: 1,
-  }
-);
-
-const projectile2 = new Projectile(
-  canvas.width / 2,
-  canvas.height / 2,
-  5,
-  "green",
-  {
-    x: -1,
-    y: -1,
-  }
-);
-
-const projectiles = [];
+function spawnEnemies() {
+  setInterval(() => {
+    console.log("go");
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+  player.projectiles.forEach((projectile) => {
     projectile.update();
   });
 }
@@ -96,9 +40,10 @@ addEventListener("click", (event) => {
     y: Math.sin(angle),
   };
 
-  projectiles.push(
+  player.projectiles.push(
     new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
   );
 });
 
 animate();
+spawnEnemies();
