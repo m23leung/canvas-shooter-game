01760670bef672198284a8a51c8c2f7320d8af98
m23leung <mark.leung89@gmail.com>
2021-08-15 15:48:26 +1000

    Implement projectile collision detection & enemy explosion effects

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // HTML5 Canvas and JavaScript Game Tutorial
 // Chris Courses
 // https://www.youtube.com/watch?v=eI9idPTT0c4&t=21s
-
+console.log(gsap);
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
@@ -11,9 +11,9 @@ canvas.height = innerHeight;
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "blue");
-//const projectiles = [];
+const player = new Player(x, y, 10, "white");
 const enemies = [];
+const particles = [];
 
 function spawnEnemies() {
   setInterval(() => {
@@ -29,57 +29,114 @@ function spawnEnemies() {
       x = Math.random() * canvas.width;
       x = Math.random() < 0.5 ? -radius : canvas.height + radius;
     }
-    const color = "green";
+
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
 
     const velocity = {
-      x: Math.cos(angle),
-      y: Math.sin(angle),
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5,
     };
 
     enemies.push(new Enemy(x, y, radius, color, velocity));
   }, 1000);
 }
 
+let animationId;
+
 function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  animationId = requestAnimationFrame(animate);
+  c.fillStyle = "rgba(0, 0, 0, 0.1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  player.projectiles.forEach((projectile) => {
+  particles.forEach((particle, particleIndex) => {
+    if (particle.alpha <= 0) {
+      particles.splice(particleIndex, 1);
+    } else {
+      particle.update();
+    }
+  });
+
+  player.projectiles.forEach((projectile, projectileIndex) => {
     projectile.update();
+
+    // remove from edges of screen
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        player.projectiles.splice(projectileIndex, 1);
+      }, 0);
+    }
   });
 
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    if (dist - enemy.radius - player.radius < 1) {
+      // End the game
+      cancelAnimationFrame(animationId);
+    }
 
     player.projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
 
-      // objects touch
+      // When projectiles touch the enemy
       if (dist - enemy.radius - projectile.radius < 1) {
-        setTimeout(() => {
-          enemies.splice(enemyIndex, 1);
-          projectiles.splice(projectileIndex, 1);
-        }, 0);
+        // create explosions
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 6),
+                y: (Math.random() - 0.5) * (Math.random() * 6),
+              }
+            )
+          );
+        }
+
+        if (enemy.radius - 10 > 5) {
+          // Shrinking effect w/ GSAP
+          gsap.to(enemy, {
+            radius: enemy.radius - 10,
+          });
+          setTimeout(() => {
+            player.projectiles.splice(projectileIndex, 1);
+          }, 0);
+        } else {
+          setTimeout(() => {
+            enemies.splice(enemyIndex, 1);
+            player.projectiles.splice(projectileIndex, 1);
+          }, 0);
+        }
       }
     });
   });
 }
 
 addEventListener("click", (event) => {
+  console.log(player.projectiles);
   const angle = Math.atan2(
     event.clientY - canvas.height / 2,
     event.clientX - canvas.width / 2
   );
 
   const velocity = {
-    x: Math.cos(angle) * 5,
-    y: Math.sin(angle) * 5,
+    x: Math.cos(angle) * 4,
+    y: Math.sin(angle) * 4,
   };
 
   player.projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity)
   );
 });
 
